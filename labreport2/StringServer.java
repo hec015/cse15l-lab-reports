1efc5e00a8645b232cae19be4d8fdf718d477dab
hec015 <146884515+hec015@users.noreply.github.com>
2023-10-17 13:47:40 -0700

    Update StringServer.java

--- labreport2/StringServer.java
+++ labreport2/StringServer.java
@@ -1,23 +1,20 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
-    int num = 0;
+    ArrayList<String> messages = new ArrayList<String>();
+    int numStrings = 0;
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
             return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
         } else {
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    messages.add(parameters[1]);
+                    return "Message added.";
                 }
             }
             return "404 Not Found!";
